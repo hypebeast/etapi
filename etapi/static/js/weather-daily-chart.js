5fbab6f6e3e7375cde14bc890f328bbb0fdeed8b
Sebastian Ruml <sebastian.ruml@gmail.com>
2015-05-02 13:56:28 +0200

    weather templates and view

---
+++ etapi/static/js/weather-daily-chart.js
@@ -0,0 +1,44 @@
+$(document).ready(function () {
+    $(function () {
+        $('#weather-daily-chart').highcharts({
+            chart: {
+                type: 'column'
+            },
+            title: {
+                text: 'Temperature'
+            },
+            colors: ['#B4C7DA', '#153E7E', '#ED561B', '#DDDF00', '#24CBE5', '#64E572', '#FF9655', '#FFF263', '#6AF9C4'],
+            xAxis: {
+                type: 'datetime'
+            },
+            yAxis: {
+                title: {
+                    text: '°C'
+                }
+            },
+            tooltip: {
+                headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+                pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+                    '<td style="padding:0"><b>{point.y:.1f} Watt</b></td></tr>',
+                footerFormat: '</table>',
+                shared: true,
+                useHTML: true
+            },
+            plotOptions: {
+                column: {
+                    pointPadding: 0.0,
+                    groupPadding: 0.5,
+                    borderWidth: 0,
+                    pointWidth: 5
+                }
+            },
+            series: [
+                {
+                    name: 'Temperature',
+                    data: data
+                }
+            ]
+        });
+    });
+});
+
---
+++ etapi/weather/views.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from datetime import datetime, timedelta
+
+from flask import (Blueprint, request, render_template)
+
+from etapi.lib.helpers import get_timestamps
+from etapi.lib.helpers import get_todays_date
+from etapi.weather.helpers import get_daily_temperature_series
+
+
+weather = Blueprint('weather', __name__, url_prefix='/weather',
+                    static_folder="../static")
+
+@weather.route("/")
+@weather.route("/<date>")
+def index(date=get_todays_date().strftime('%Y-%m-%d')):
+    try:
+        current_date = datetime.strptime(date, '%Y-%m-%d')
+    except ValueError, TypeError:
+        current_date = get_todays_date()
+
+    yesterday = current_date - timedelta(days=1)
+    tomorrow = current_date + timedelta(days=1)
+
+    temp_data = get_daily_temperature_series(current_date)
+    timestamps_temp = get_timestamps(temp_data)
+
+    daily_chart_data = [list(x) for x in zip(timestamps_temp, [(float(d.temp or 0 )) for d in temp_data])]
+
+    return render_template("weather/weather.html",
+                            today=current_date, yesterday=yesterday, tomorrow=tomorrow,
+                            data=daily_chart_data)

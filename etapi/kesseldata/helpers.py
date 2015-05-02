135866ad8786ae8bc5756f3271ba80e7ff439ff9
Sebastian Ruml <sebastian.ruml@gmail.com>
2015-05-02 17:18:09 +0200

    handle empty return

--- etapi/kesseldata/helpers.py
+++ etapi/kesseldata/helpers.py
@@ -36,31 +36,50 @@ def get_pellets_kessel_stock():
     """
     Returns the kessel pellets stock.
     """
-    return Kessel.query.with_entities(Kessel.pellets_stock).filter(
-        func.strftime('%Y-%m-%d', Kessel.created_at) == datetime.utcnow().strftime('%Y-%m-%d')).order_by(
-        Kessel.id.desc()).first().pellets_stock
+    result = Kessel.query.with_entities(Kessel.pellets_stock).filter(
+                func.strftime('%Y-%m-%d', Kessel.created_at) == datetime.utcnow().strftime('%Y-%m-%d')).order_by(
+                Kessel.id.desc()).first()
+
+    if not result:
+        return None
+
+    return result.pellets_stock
 
 def get_pellets_total_stock():
     """
     Returns the total pellets stock.
     """
-    return Lager.query.with_entities(Lager.stock).filter(
-        func.strftime('%Y-%m-%d', Kessel.created_at) == datetime.utcnow().strftime('%Y-%m-%d')).order_by(
-        Lager.id.desc()).first().stock
+    result = Lager.query.with_entities(Lager.stock).filter(
+                func.strftime('%Y-%m-%d', Kessel.created_at) == datetime.utcnow().strftime('%Y-%m-%d')).order_by(
+                Lager.id.desc()).first()
+
+    if not result:
+        return None
+
+    return result.stock
 
 def get_operating_hours_total():
     """
     Returns the total operating hours.
     """
-    return Kessel.query.with_entities(Kessel.operating_hours).filter(
+    result = Kessel.query.with_entities(Kessel.operating_hours).filter(
         func.strftime('%Y-%m-%d', Kessel.created_at) == datetime.utcnow().strftime('%Y-%m-%d')).order_by(
-        Kessel.id.desc()).first().operating_hours
+        Kessel.id.desc()).first()
+
+    if not result:
+        return None
+
+    return result.operating_hours
 
 def get_operating_hours_last_n_days(n=7):
     """
     Returns the operating hours for the last n days.
     """
-    return Kessel.query.with_entities((func.max(Kessel.operating_hours) - func.min(Kessel.operating_hours)).label('operating_hours')).filter(
-        func.strftime('%Y-%m-%d', Kessel.created_at) > (datetime.utcnow() - timedelta(days=n))).first().operating_hours
+    result = Kessel.query.with_entities((func.max(Kessel.operating_hours) - func.min(Kessel.operating_hours)).label('operating_hours')).filter(
+        func.strftime('%Y-%m-%d', Kessel.created_at) > (datetime.utcnow() - timedelta(days=n))).first()
 
+    if not result:
+        return None
+
+    return result.operating_hours
 

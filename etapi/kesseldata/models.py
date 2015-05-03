21fff115827e082883208da7b6180714e062f112
Sebastian Ruml <sebastian.ruml@gmail.com>
2015-05-03 12:48:34 +0200

    read new data from the kessel

--- etapi/kesseldata/models.py
+++ etapi/kesseldata/models.py
@@ -21,6 +21,22 @@ class Kessel(SurrogatePK, Model):
     reservoir_capacity = Column(db.Integer(), nullable=True)
     usage_since_deashing = Column(db.Integer(), nullable=True)
     usage_since_box_exhaustion = Column(db.Integer(), nullable=True)
+    pressure = Column(db.Float(), nullable=True)
+    temperature = Column(db.Integer(), nullable=True)
+    feed_line_temperature = Column(db.Integer(), nullable=True)
+    exhaust_temperature = Column(db.Integer(), nullable=True)
+    exhaust_blower = Column(db.Integer(), nullable=True)
+    residual_oxygen = Column(db.Float(), nullable=True)
+
+
+class Puffer(SurrogatePK, Model):
+    __tablename__ = 'puffer_data'
+
+    id = Column(db.Integer(), nullable=False, primary_key=True)
+    created_at = Column(db.DateTime(), nullable=False, default=dt.datetime.utcnow)
+    temperature_top = Column(db.Integer(), nullable=True)
+    temperature_bottom = Column(db.Integer(), nullable=True)
+    hot_water_storage_temp = Column(db.Integer(), nullable=True)
 
 
 class Lager(SurrogatePK, Model):
--- scripts/crawler/crawler.py
+++ scripts/crawler/crawler.py
@@ -10,7 +10,7 @@ from etapi.app import create_app
 from etapi.extensions import db
 from etapi.settings import DevConfig, ProdConfig
 from etapi.weather.models import Weather
-from etapi.kesseldata.models import Kessel, Lager
+from etapi.kesseldata.models import Kessel, Lager, Puffer
 
 if os.environ.get("ETAPI_ENV") == 'prod':
     app = create_app(ProdConfig)
@@ -51,13 +51,29 @@ def kesselData():
     behaelterinhalt = int(getStrValueOrNone(getUserVar("/40/10021/0/0/12011")))
     verbrauchseitentaschung = int(getStrValueOrNone(getUserVar("/40/10021/0/0/12012")))
     verbrauchseitascheboxleeren = int(getStrValueOrNone(getUserVar("/40/10021/0/0/12013")))
+    pressure = getStrValueOrNone(getUserVar("/40/10021/0/0/12180"))
+    if pressure:
+        pressure = float(pressure.replace(',', '.'))
+    temperature = int(getStrValueOrNone(getUserVar("/40/10021/0/0/12161")))
+    feed_line_temperature = int(getStrValueOrNone(getUserVar("/40/10021/0/0/12241")))
+    exhaust_temperature = int(getStrValueOrNone(getUserVar("/40/10021/0/0/12162")))
+    exhaust_blower = int(getStrValueOrNone(getUserVar("/40/10021/0/0/12165")))
+    residual_oxygen = getValueOrNone(getUserVar("/40/10021/0/0/12164"))
+    if residual_oxygen:
+        residual_oxygen = float(residual_oxygen.replace(',', '.'))
 
     kessel = Kessel(operating_hours=vollaststunden,
                     pellets_total=gesamtverbrauch,
                     usage_since_service=verbrauchseitwartung,
                     reservoir_capacity=behaelterinhalt,
                     usage_since_deashing=verbrauchseitentaschung,
-                    usage_since_box_exhaustion=verbrauchseitascheboxleeren)
+                    usage_since_box_exhaustion=verbrauchseitascheboxleeren,
+                    pressure=pressure,
+                    temperature=temperature,
+                    feed_line_temperature=feed_line_temperature,
+                    exhaust_temperature=exhaust_temperature,
+                    exhaust_blower=exhaust_blower,
+                    residual_oxygen=residual_oxygen)
 
     with app.app_context():
         db.session.add(kessel)
@@ -69,6 +85,20 @@ def lagerData():
     with app.app_context():
         db.session.add(lager)
         db.session.commit()
+
+def pufferData():
+    temperature_top = int(getStrValueOrNone(getUserVar("/120/10251/0/0/12242")))
+    temperature_bottom = int(getStrValueOrNone(getUserVar("/120/10251/0/0/12244")))
+    hot_water_storage_temp = int(getStrValueOrNone(getUserVar("/120/10251/0/0/12271")))
+
+    puffer = Puffer(temperature_top=temperature_top,
+                    temperature_bottom=temperature_bottom,
+                    hot_water_storage_temp=hot_water_storage_temp)
+
+    with app.app_context():
+        db.session.add(puffer)
+        db.session.commit()
+
 
 def weatherData():
     temp = getStrValueOrNone(getUserVar("/40/10241/0/0/12197"))
@@ -86,6 +116,7 @@ def main():
     weatherData()
     kesselData()
     lagerData()
+    pufferData()
 
     end = datetime.utcnow()
     print "%s - DONE" % ((end.strftime('%Y-%m-%d %H:%M:%S')))
